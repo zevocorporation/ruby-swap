ee8b9ff76f47d39c15af02f6943d9f8f66ab641f
Arunram07 <arunram@zevocorp.in>
2021-06-01 12:39:38 +0530

    'header_test'

--- src/App.js
+++ src/App.js
@@ -3,8 +3,31 @@ function App() {
     <div className="header">
       <img className="logo" />
       <div className="controls">
-        <div className="link"></div>
+        <div className="menu">
+          <a href="/" id="home">
+            Home
+          </a>
+          <a href="/" id="trade">
+            Trade
+          </a>
+          <a href="/" id="farms">
+            Farms
+          </a>
+          <a href="/" id="pools">
+            Pools
+          </a>
+          <a href="/" id="productions">
+            Productions
+          </a>
+          <a href="/" id="collectibles">
+            Collectibles
+          </a>
+          <a href="/" id="more">
+            More
+          </a>
+        </div>
         <button>connect</button>
+        <img className="hamburger" />
       </div>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import App from "./App";
 
-import { configure, shallow } from "enzyme";
+import { configure, mount, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
@@ -26,5 +26,31 @@ describe("header rendering", () => {
   test("menu is rendered", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.exists(".menu")).toBe(true);
+  });
+  test("hamburger is rendered", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists(".hamburger")).toBe(true);
+  });
+  test("menu has nested children", () => {
+    const wrapper = shallow(<App />);
+    const result = wrapper.find(".menu").children().length;
+    expect(result).toEqual(7);
+  });
+  test("menu link text rendered", () => {
+    const wrapper = shallow(<App />);
+    const menu1 = wrapper.find(".menu").childAt(0).text();
+    expect(menu1).toEqual("Home");
+    const menu2 = wrapper.find(".menu").childAt(1).text();
+    expect(menu2).toEqual("Trade");
+    const menu3 = wrapper.find(".menu").childAt(2).text();
+    expect(menu3).toEqual("Farms");
+    const menu4 = wrapper.find(".menu").childAt(3).text();
+    expect(menu4).toEqual("Pools");
+    const menu5 = wrapper.find(".menu").childAt(4).text();
+    expect(menu5).toEqual("Productions");
+    const menu6 = wrapper.find(".menu").childAt(5).text();
+    expect(menu6).toEqual("Collectibles");
+    const menu7 = wrapper.find(".menu").childAt(6).text();
+    expect(menu7).toEqual("More");
   });
 });
