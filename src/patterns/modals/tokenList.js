bed6f9be6de4f52b722ea22ae37de913ec86372b
zero <sam@zevocorp.in>
2021-06-16 11:04:57 +0530

    TokenListAdded

---
+++ src/patterns/modals/tokenList.js
@@ -0,0 +1,11 @@
+export const Token = [
+    {
+        Name: "WETH",
+        address: "0xc778417e063141139fce010982780140aa0cd5ab",
+        img:"" 
+    },
+    {
+        Name: "TST",
+        address: "0xBFc358F36Be8eB80Cc73b39f3E99e09906bD44A6",
+        img:""
+    }]
