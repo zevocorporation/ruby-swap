04e4d069210bcf90eac3838bc9fe4529598b2981
zero <sam@zevocorp.in>
2021-06-16 11:07:59 +0530

    TokenListAdded

--- src/patterns/modals/tokenList.js
+++ src/patterns/modals/tokenList.js
@@ -9,3 +9,6 @@ export const Token = [
         address: "0xBFc358F36Be8eB80Cc73b39f3E99e09906bD44A6",
         img:""
     }]
+
+    //UnlockButton => swap (after connected to metamask)
+    //add handleInput to the 1st input
